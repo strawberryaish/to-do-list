b6d1623080f8ee5a27f8dc697988d2d31939995e
strawberryaish <trismorgenstern@gmail.com>
2024-03-24 22:00:14 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      primary: '#520abd',
+      secondary: '#8445ff',
+      darkblue1: '#781fff',
+      darkblue2: '#2b0372',
+      lightblue1: '#bfaaff',
+      lightblue2: '#ebe6ff',
+
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
